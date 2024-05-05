85ecd3b07fe90ab51b87c5cafc978209aaa9419e
Andy Lulham <a.lulham@gmail.com>
2024-05-05 10:49:03 +0100

    Refactor, to avoid writing JSON this horrible way
    Doesn’t actually help, and we’re not going to run into memory
    issues anyway.

--- ff_notes/fetch_statuses.py
+++ ff_notes/fetch_statuses.py
@@ -7,40 +7,40 @@ helpful = "CURRENTLY_RATED_HELPFUL"
 unhelpful = "CURRENTLY_RATED_NOT_HELPFUL"
 
 
+note_statuses = {}
+
 if __name__ == "__main__":
     with open("output/_data/notes.csv") as fh:
         all_note_ids = [row["note_id"] for row in csv.DictReader(fh)]
 
-    started = False
-    with open("output/_data/statuses.json", "w") as fh:
-        fh.write("{")
-        for row in get_generator("noteStatusHistory"):
-            output = {}
-            if row["noteId"] not in all_note_ids:
+    for row in get_generator("noteStatusHistory"):
+        note_status = {}
+        if row["noteId"] not in all_note_ids:
+            continue
+        if row["currentStatus"] == unhelpful:
+            # this is currently rated unhelpful,
+            # so we’ll exclude it
+            note_status["status"] = "unhelpful"
+        else:
+            if (
+                row["firstNonNMRStatus"] != helpful
+                and row["mostRecentNonNMRStatus"] != helpful
+            ):
                 continue
-            if row["currentStatus"] == unhelpful:
-                # this is currently rated unhelpful,
-                # so we’ll exclude it
-                output["status"] = "unhelpful"
+            if row["firstNonNMRStatus"] == helpful:
+                from_ts = to_isoformat(row["timestampMillisOfFirstNonNMRStatus"])
             else:
-                if (
-                    row["firstNonNMRStatus"] != helpful
-                    and row["mostRecentNonNMRStatus"] != helpful
-                ):
-                    continue
-                if row["firstNonNMRStatus"] == helpful:
-                    from_ts = to_isoformat(row["timestampMillisOfFirstNonNMRStatus"])
-                else:
-                    from_ts = to_isoformat(row["timestampMillisOfLatestNonNMRStatus"])
-                output["from"] = str(from_ts)
-                if row["currentStatus"] != helpful:
-                    # this timestamp often doesn’t appear to be useful.
-                    # I suspect because there are cases where the status is disputed,
-                    # so the current status changes frequently
-                    output["to"] = str(to_isoformat(row["timestampMillisOfCurrentStatus"]))
-            if started:
-                fh.write(",")
-            fh.write(f'"{row["noteId"]}":')
-            fh.write(json.dumps(output))
-            started = True
-        fh.write("}")
+                from_ts = to_isoformat(row["timestampMillisOfLatestNonNMRStatus"])
+            note_status["from"] = str(from_ts)
+            if row["currentStatus"] != helpful:
+                # this timestamp often doesn’t appear to be useful.
+                # I suspect because there are cases where the status
+                # is disputed, so the current status changes frequently
+                note_status["to"] = str(
+                    to_isoformat(row["timestampMillisOfCurrentStatus"])
+                )
+        note_statuses[row["noteId"]] = note_status
+
+
+with open("output/_data/statuses.json", "w") as fh:
+    json.dump(note_statuses, fh)
