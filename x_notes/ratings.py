2728daf192d6e506bc873fba82112e4461ed9c5a
Andy Lulham <a.lulham@gmail.com>
2024-05-21 07:53:38 +0100

    ignore helpfulness; just store number of ratings

--- x_notes/ratings.py
+++ x_notes/ratings.py
@@ -3,13 +3,11 @@ from typing import Any
 from .helpers import load_notes, save_notes
 from .tsv import get_generator
 
-helpfulness_lookup = {"HELPFUL": 0, "SOMEWHAT_HELPFUL": 1, "NOT_HELPFUL": 2}
+# helpfulness_lookup = {"HELPFUL": 0, "SOMEWHAT_HELPFUL": 1, "NOT_HELPFUL": 2}
 
 
 def add_ratings(notes: dict[str, dict[str, Any]]) -> None:
-    for note in notes.values():
-        # clear all previous ratings first
-        del note["ratings"]
+    tweet_ratings = {}
     index = 0
     while True:
         try:
@@ -19,11 +17,15 @@ def add_ratings(notes: dict[str, dict[str, Any]]) -> None:
         for row in gen:
             if row["noteId"] in notes:
                 note_id = row["noteId"]
-                rating = helpfulness_lookup[row["helpfulnessLevel"]]
-                if "rating" not in notes[note_id]:
-                    notes[note_id]["rating"] = [0, 0, 0]
-                notes[note_id]["rating"][rating] += 1
+                tweet_id = notes[note_id]["tweet_id"]
+                # rating = helpfulness_lookup[row["helpfulnessLevel"]]
+                if tweet_id not in tweet_ratings:
+                    tweet_ratings[tweet_id] = 1
+                tweet_ratings[tweet_id] += 1
         index += 1
+    for note in notes.values():
+        if note["tweet_id"] in tweet_ratings:
+            note["rating"] = tweet_ratings[note["tweet_id"]]
     save_notes(notes)
 
 
